832ac916326afbb6418d95deeb899944f97deba9
Anthony Tse <tsekwokyan@gmail.com>
2016-01-03 01:13:00 +0800

    Add Ch.3

--- Ch_02/src/ResourceHolder.cpp
+++
@@ -1,6 +0,0 @@
-#include "ResourceHolder.hpp"
-
-ResourceHolder::ResourceHolder()
-{
-
-}
---
+++ Ch_03/src/Game.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+#include "ResourceHolder.hpp"
+
+enum class TexturesID
+{
+    Landscape,
+    Airplane,
+};
+
+class Game
+{
+public:
+    Game();
+    void run();
+
+private:
+    void processEvents();
+    void update(sf::Time deltaTime);
+    void render();
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
+private:
+    static const float		PlayerSpeed;
+	static const sf::Time	TimePerFrame;
+
+    sf::RenderWindow mWindow;
+    ResourceHolder<sf::Texture, TexturesID> mTextures;
+    sf::Sprite mPlayer;
+    sf::Sprite mLandscape;
+    bool mIsMovingUp, mIsMovingDown, mIsMovingLeft, mIsMovingRight;
+};
